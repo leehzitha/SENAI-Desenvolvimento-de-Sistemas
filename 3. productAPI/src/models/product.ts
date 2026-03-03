bc6955b01287ecec18102cabb38886d34777338d
Leehzitha <leticia.burlinski@gmail.com>
2026-03-03 11:33:12 -0300

    Started productAPI, started setting models

---
+++ 3. productAPI/src/models/product.ts
@@ -0,0 +1,10 @@
+import mongoose, { Schema, Document, Date } from 'mongoose';
+
+interface IProduct extends Document {
+    name : { type: String, required: true },
+    description : { type: String, required: false},
+    price : { type: Number, required: true },
+    stock : { type: Number, default: 0 },
+    category : { type: String, required: false},
+    createdAt : { type: Date }
+}
---
+++ 3. productAPI/src/routes/product.ts
@@ -0,0 +1,8 @@
+import express, { Router } from 'express';
+
+
+const router: Router = express.Router();
+
+router
+
+export default router;
